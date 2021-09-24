04ab833a767073861671c2597ec24d483fa845cf
Brama Udi <bramaudi.al@gmail.com>
2021-09-24 07:10:53 +0700

    feat: add tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { render } from "solid-js/web"
 import { Router, useRoutes, hashIntegration } from "solid-app-router"
 import routes from "./routes" // <- auto-generated
 import NavLinks from "./components/NavLinks"
+import "./tailwind.css"
 
 const App = () => {
 	const Routes = useRoutes(routes)
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './src/pages/**/*.tsx',
+  ],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
